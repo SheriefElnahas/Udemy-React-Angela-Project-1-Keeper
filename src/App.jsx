562cf4d214cdc34afc5c6253d7857e0c38ab90be
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-28 23:51:37 +0200

    Header Complete First Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-function App() {
- 
+import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
 
+function App() {
   return (
     <div className="App">
-      <h1>Hello, World</h1>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   )
 }
 
 export default App
+
+
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+
+
+function Note() {
+    return (
+        <div className="Note">
+            <h1>Note Title</h1>
+            <p>Note Content</p>
+        </div>
+    );
+}
+
+export default Note;
